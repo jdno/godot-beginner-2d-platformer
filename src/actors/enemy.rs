03338bbf65bdf3ca5f38123199996970633a45c0
Jan David <jdno@jdno.dev>
2022-12-18 19:13:32 +0100

    Move the enemies
    The enemy type has been extended to support movement. By default,
    enemies move left towards the player until they hit an obstacle. When
    they collide with something, they turn around and move back in the
    opposite direction.

--- src/actors/enemy.rs
+++ src/actors/enemy.rs
@@ -1,19 +1,52 @@
+use std::f64::consts::FRAC_PI_4;
 use std::fmt::Display;
 
 use gdnative::prelude::*;
 
-#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default, NativeClass)]
+use crate::actors::FLOOR_NORMAL;
+
+#[derive(Copy, Clone, PartialEq, Debug, Default, NativeClass)]
 #[inherit(KinematicBody2D)]
-pub struct Enemy {}
+pub struct Enemy {
+    #[property]
+    gravity: f32,
+
+    #[property]
+    speed: f32,
+
+    velocity: Vector2,
+}
 
 impl Enemy {
     fn new(_owner: &KinematicBody2D) -> Self {
-        Enemy {}
+        Enemy {
+            gravity: 1000.0,
+            speed: 400.0,
+            velocity: Vector2::ZERO,
+        }
     }
 }
 
 #[methods]
-impl Enemy {}
+impl Enemy {
+    #[method]
+    fn _ready(&mut self, #[base] _base: &KinematicBody2D) {
+        self.velocity.x = -self.speed
+    }
+
+    #[method]
+    fn _physics_process(&mut self, #[base] owner: &KinematicBody2D, delta: f32) {
+        self.velocity.y += self.gravity * delta;
+
+        if owner.is_on_wall() {
+            self.velocity.x *= -1.0;
+        }
+
+        self.velocity.y = owner
+            .move_and_slide(self.velocity, FLOOR_NORMAL, false, 4, FRAC_PI_4, true)
+            .y;
+    }
+}
 
 impl Display for Enemy {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
