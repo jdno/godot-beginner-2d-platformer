58112bb4325e795d40b9484fbfa3bd5882e0d22b
Jan David <jdno@jdno.dev>
2022-12-18 15:26:27 +0100

    Move the player
    The first step towards controlling the player has been implemented.
    Players can use the `A` or `Left` keys to move the player character to
    the left, and `D` and `Right` to move them to the right.
    
    The implementation for the movement has been fully moved from the
    `Actor` trait to the player class. Once we know how the enemies will
    move, we can move shared behavior back into the trait.

--- src/actors/player.rs
+++ src/actors/player.rs
@@ -1,20 +1,40 @@
+use std::f64::consts::FRAC_PI_4;
 use std::fmt::Display;
 
 use gdnative::prelude::*;
 
 use crate::actors::Actor;
 
+const GRAVITY: f32 = 1000.0;
+
 #[derive(Copy, Clone, PartialEq, Debug, Default, NativeClass)]
 #[inherit(KinematicBody2D)]
 pub struct Player {
+    #[property]
+    speed: f32,
     velocity: Vector2,
 }
 
 impl Player {
     pub fn new(_base: &KinematicBody2D) -> Self {
         Self {
+            speed: 800.0,
             velocity: Vector2::ZERO,
         }
+    }
+
+    fn calculate_direction(&self) -> Vector2 {
+        let input = Input::godot_singleton();
+
+        let move_right_input = input.get_action_strength("move_right", false) as f32;
+        let move_left_input = input.get_action_strength("move_left", false) as f32;
+
+        Vector2::new(move_right_input - move_left_input, 0.0)
+    }
+
+    fn calculate_gravity(&self, delta: f32) -> Vector2 {
+        let gravity = self.velocity.y + GRAVITY * delta;
+        Vector2::new(0.0, gravity)
     }
 }
 
@@ -22,19 +42,17 @@ impl Player {
 impl Player {
     #[method]
     fn _physics_process(&mut self, #[base] owner: &KinematicBody2D, delta: f32) {
-        self.physics_process(owner, delta);
-    }
-}
+        let gravity = self.calculate_gravity(delta);
+        let direction = self.calculate_direction();
 
-impl Actor for Player {
-    fn velocity(&self) -> &Vector2 {
-        &self.velocity
-    }
-
-    fn set_velocity(&mut self, velocity: Vector2) {
+        let velocity = gravity + direction * self.speed;
         self.velocity = velocity;
+
+        owner.move_and_slide(velocity, Vector2::ZERO, false, 4, FRAC_PI_4, true);
     }
 }
+
+impl Actor for Player {}
 
 impl Display for Player {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
