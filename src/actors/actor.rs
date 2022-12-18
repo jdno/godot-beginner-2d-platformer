7b03050dd3bdbc6293a058c2913e34fe01be249f
Jan David <jdno@jdno.dev>
2022-12-18 14:33:25 +0100

    Add gravity to the player (#9)
    The player is now effected by gravity and falls down. This feature
    requires the kinetic body to know its previous speed. The player has a
    field now to track its own velocity, and the trait for actors has been
    extended to provide getter and setter methods for it.

--- src/actors/actor.rs
+++ src/actors/actor.rs
@@ -2,18 +2,18 @@ use std::f64::consts::FRAC_PI_4;
 
 use gdnative::prelude::*;
 
-pub trait Actor {
-    fn physics_process(&mut self, owner: &KinematicBody2D, _delta: f32) {
-        let horizontal_movement: Vector2 = Vector2::new(300.0, 0.0);
-        let vertical_movement: Vector2 = Vector2::ZERO;
+const GRAVITY: f32 = 3000.0;
 
-        owner.move_and_slide(
-            horizontal_movement,
-            vertical_movement,
-            false,
-            4,
-            FRAC_PI_4,
-            false,
-        );
+pub trait Actor {
+    fn velocity(&self) -> &Vector2;
+    fn set_velocity(&mut self, velocity: Vector2);
+
+    fn physics_process(&mut self, owner: &KinematicBody2D, delta: f32) {
+        let gravity = self.velocity().y + GRAVITY * delta;
+
+        let velocity = Vector2::new(self.velocity().x, gravity);
+        self.set_velocity(velocity);
+
+        owner.move_and_slide(velocity, Vector2::ZERO, false, 4, FRAC_PI_4, true);
     }
 }
